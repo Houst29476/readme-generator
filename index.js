e200221d8ae916194d4998fdc182b44f49192ad5
Bradley Boyd <bradley.boyd@gmail.com>
2022-02-18 16:21:25 -0600

    added functionality to capture user input

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello Node!');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// ----- Provides user interface and the inquiry session flow ----- //
+
+inquirer
+    .prompt (questions)
+    .then (answers => {
+
+        const { username, title, description, installation, useage, command, command2, credit, license } = answers;
+
+        const template = createReadMe(username, title, description, installation, useage, command, command2, credit, license);
+
+        fs.writeFile('README.md', template,
+        (err) => {
+            if (err) throw err;
+            console.log('Generating README file...');
+        });
+    })
